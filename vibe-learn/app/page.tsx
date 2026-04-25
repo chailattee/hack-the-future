b1d731debc9b2bb73fda006f26ba9128b9748d78
chailattee <charlottenorris1111@gmail.com>
2026-04-25 11:49:08 -0400

    remove duplicate vibe-learn folder

--- vibe-learn/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import VibeLearEditor from './components/VibeLearEditor'
-
-export default function Home() {
-  return (
-    <div className="h-full min-h-0 flex flex-col flex-1">
-      <VibeLearEditor />
-    </div>
-  )
-}
